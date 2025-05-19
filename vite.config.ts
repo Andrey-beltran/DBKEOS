25f110b7c17555dc17ce4c54a7c6cc77e1a1dc67
JohaAndrey <99775462+Andrey-beltran@users.noreply.github.com>
2025-05-19 09:06:41 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/DBKEOS/", // 👈 Agrega esta línea
+  base: "/DBKEOS/", // 👈 ESTA LÍNEA es esencial para GitHub Pages
 
   server: {
     host: "::",
